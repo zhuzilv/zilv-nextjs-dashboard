bcbfd93a7406e892d3e3bba30316d28eade8b741
zhujiahong <vermilionand@gmail.com>
2024-06-04 21:19:02 +0800

    init project

---
+++ app/ui/fonts.ts
@@ -0,0 +1,12 @@
+import { Inter, Montserrat, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: '400',
+});
+
+export const mont = Montserrat({
+  subsets: ['latin'],
+});
